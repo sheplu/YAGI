8729ba31abcd2574dba0651f98fdb6de3337d711
Jean Burellier <jean.burellier@gmail.com>
2025-07-25 18:44:00 +0200

    feat: list repositories

--- api/repository.js
+++ api/repository.js
@@ -12,3 +12,29 @@ export async function getRepository(owner, repository) {
     
     return request.json();
 };
+
+export async function listRepositories(owner) {
+	try {
+		let page = 1;
+		let continueLoop = true;
+		const repositories = [];
+		while (continueLoop) {
+			const url = `${GITHUB_URL}/orgs/${owner}/repos?per_page=100&page=${page}`;
+			const request = await fetch(url, {
+				headers: {
+					Accept: 'application/vnd.github.v3+json',
+					Authorization: `Bearer ${token}`,
+				},
+			});
+            const result = await request.json();
+			repositories.push(...result);
+			page++;
+			if (result.length < 100) {
+				continueLoop = false;
+			}
+		}
+		return repositories;
+	} catch (error) {
+		console.error(error);
+	}
+};
