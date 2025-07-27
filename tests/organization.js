d1ca6d619380422983e39cfe2b68a02fcd09515d
Jean Burellier <jean.burellier@gmail.com>
2025-07-27 21:00:40 +0200

    feat: create repository for organisation

--- tests/organization.js
+++ tests/organization.js
@@ -1,4 +1,19 @@
-import { listTeams } from "../main.js";
+import { createRepository, listTeams } from "../main.js";
 
 const organizationTeam = await listTeams('not-organisation');
 console.log(organizationTeam.length);
+
+const createRepo = await createRepository('not-organisation', {
+            "name": 'My-new-repo',
+            "description": "This is your first repository",
+            "homepage": "https://github.com",
+            "private": true,
+            "has_issues": true,
+            "has_projects": true,
+            "has_wiki": true,
+            "allow_squash_merge": false,
+            "allow_merge_commit": false,
+            "delete_branch_on_merge": true,
+            "auto_init": true
+        });
+console.log(createRepo);
