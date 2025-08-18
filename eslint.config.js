428187c9e9b48e3e4a6bd4f3ca258625f7ab6c9b
Jean Burellier <jean.burellier@gmail.com>
2025-08-18 23:18:45 +0200

    feat: use custom linter

--- eslint.config.js
+++ eslint.config.js
@@ -2,12 +2,14 @@ import js from "@eslint/js";
 import globals from "globals";
 import markdown from "@eslint/markdown";
 import { defineConfig } from "eslint/config";
+import stylistic from '@stylistic/eslint-plugin'
+import myConfig from "@sheplu/eslint-config/src/stylistic.js";
 
 export default defineConfig([
 	{
 		files: ["**/*.{js,mjs,cjs}"],
-		plugins: { js },
-		extends: ["js/recommended"],
+		plugins: { js, "@stylistic": stylistic },
+		extends: [ "js/recommended", myConfig ],
 		languageOptions: {
 			globals: globals.node
 		}
