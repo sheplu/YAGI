5cbbffc063e7998e4434064ff075bacc2e02a282
Jean Burellier <jean.burellier@gmail.com>
2025-08-27 20:55:13 +0200

    feat: get target branch from repository

--- api/repository.js
+++ api/repository.js
@@ -256,6 +256,18 @@ export async function listBranches(owner, repository) {
 	return request.json();
 };
 
+export async function getBranch(owner, repository, branch) {
+	const url = `${GITHUB_URL}/repos/${owner}/${repository}/branches/${branch}`;
+	const request = await fetch(url, {
+		headers: {
+			Accept: 'application/vnd.github.v3+json',
+			Authorization: `Bearer ${GITHUB_TOKEN}`,
+		},
+	});
+
+	return request.json();
+};
+
 export async function listReleases(owner, repository) {
 	const url = `${GITHUB_URL}/repos/${owner}/${repository}/releases`;
 	const request = await fetch(url, {
