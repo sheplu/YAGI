f51f4456c4148c6cae0c18e3ed4727f1c49aa3f1
Jean Burellier <jean.burellier@gmail.com>
2025-09-29 07:36:22 +0200

    feat: add repository method for list contributors

--- src/repositories/repositories.js
+++ src/repositories/repositories.js
@@ -1,5 +1,11 @@
+import {
+	BASE_COUNTER,
+	DEFAULT_INCREMENT,
+	GITHUB_PAGE_LENGTH,
+	GITHUB_URL,
+} from '../utils/const.js';
 import { GITHUB_TOKEN } from '../utils/token.js';
-import { GITHUB_URL } from '../utils/const.js';
+import { logger } from '../utils/logger.js';
 
 export async function getRepository(owner, repository) {
 	const url = `${GITHUB_URL}/repos/${owner}/${repository}`;
@@ -59,4 +65,37 @@ export async function getTopics(owner, repository) {
 	});
 
 	return request.json();
+};
+
+export async function listContributors(owner, repository) {
+	try {
+		let page = BASE_COUNTER;
+		let continueLoop = true;
+		const contributors = [];
+
+		while (continueLoop) {
+			const url = `${GITHUB_URL}/repos/${owner}/${repository}/contributors
+				?per_page=${GITHUB_PAGE_LENGTH}&page=${page}`;
+			// eslint-disable-next-line no-await-in-loop
+			const request = await fetch(url, {
+				headers: {
+					Accept: 'application/vnd.github.v3+json',
+					Authorization: `Bearer ${GITHUB_TOKEN}`,
+				},
+			});
+			// eslint-disable-next-line no-await-in-loop
+			const result = await request.json();
+
+			contributors.push(...result);
+			page = page + DEFAULT_INCREMENT;
+			if (result.length < GITHUB_PAGE_LENGTH) {
+				continueLoop = false;
+			}
+		}
+
+		return contributors;
+	} catch (error) {
+		logger.error(error);
+		throw new Error(`Error listing contributors for ${owner}/${repository}`, { cause: error });
+	}
 };
