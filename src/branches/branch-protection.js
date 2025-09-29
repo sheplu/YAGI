ad986ea477d09863fc260175132988a08fa1e12f
Jean Burellier <jean.burellier@gmail.com>
2025-09-29 08:01:26 +0200

    feat: add branches method to get branch protection status

---
+++ src/branches/branch-protection.js
@@ -0,0 +1,14 @@
+import { GITHUB_TOKEN } from '../utils/token.js';
+import { GITHUB_URL } from '../utils/const.js';
+
+export async function getBranchProtection(owner, repository, branch) {
+	const url = `${GITHUB_URL}/repos/${owner}/${repository}/branches/${branch}/protection`;
+	const request = await fetch(url, {
+		headers: {
+			Accept: 'application/vnd.github.v3+json',
+			Authorization: `Bearer ${GITHUB_TOKEN}`,
+		},
+	});
+
+	return request.json();
+};
