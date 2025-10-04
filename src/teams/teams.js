46d954701b71fe30a82e249f5c501222f910f7fc
Jean Burellier <jean.burellier@gmail.com>
2025-10-04 19:30:14 +0200

    feat: add teams method for list repositories of teams

--- src/teams/teams.js
+++ src/teams/teams.js
@@ -41,3 +41,36 @@ export async function listTeams(owner) {
 		throw new Error(`Error listing teams for ${owner}`, { cause: error });
 	}
 };
+
+export async function listTeamsRepositories(owner, team) {
+	try {
+		let page = BASE_COUNTER;
+		let continueLoop = true;
+		const repositories = [];
+
+		while (continueLoop) {
+			const url = `${GITHUB_URL}/orgs/${owner}/teams/${team}/repos
+				?per_page=${GITHUB_PAGE_LENGTH}&page=${page}`;
+			// eslint-disable-next-line no-await-in-loop
+			const request = await fetch(url, {
+				headers: {
+					Accept: 'application/vnd.github.v3+json',
+					Authorization: `Bearer ${GITHUB_TOKEN}`,
+				},
+			});
+			// eslint-disable-next-line no-await-in-loop
+			const result = await request.json();
+
+			repositories.push(...result);
+			page = page + DEFAULT_INCREMENT;
+			if (result.length < GITHUB_PAGE_LENGTH) {
+				continueLoop = false;
+			}
+		}
+
+		return repositories;
+	} catch (error) {
+		logger.error(error);
+		throw new Error(`Error listing repositories for ${owner} ${team}`, { cause: error });
+	}
+};
