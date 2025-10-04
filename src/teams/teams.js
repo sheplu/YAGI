d7841f481f28df1b4811c744ebc51e74c18f4edb
Jean Burellier <jean.burellier@gmail.com>
2025-10-04 19:50:15 +0200

    feat: add teams method for list child teams

--- src/teams/teams.js
+++ src/teams/teams.js
@@ -74,3 +74,36 @@ export async function listTeamsRepositories(owner, team) {
 		throw new Error(`Error listing repositories for ${owner} ${team}`, { cause: error });
 	}
 };
+
+export async function listChildTeams(owner, team) {
+	try {
+		let page = BASE_COUNTER;
+		let continueLoop = true;
+		const teams = [];
+
+		while (continueLoop) {
+			const url = `${GITHUB_URL}/orgs/${owner}/teams/${team}/teams
+				?per_page=${GITHUB_PAGE_LENGTH}&page=${page}`;
+			// eslint-disable-next-line no-await-in-loop
+			const request = await fetch(url, {
+				headers: {
+					Accept: 'application/vnd.github.v3+json',
+					Authorization: `Bearer ${GITHUB_TOKEN}`,
+				},
+			});
+			// eslint-disable-next-line no-await-in-loop
+			const result = await request.json();
+
+			teams.push(...result);
+			page = page + DEFAULT_INCREMENT;
+			if (result.length < GITHUB_PAGE_LENGTH) {
+				continueLoop = false;
+			}
+		}
+
+		return teams;
+	} catch (error) {
+		logger.error(error);
+		throw new Error(`Error listing teams for ${owner}`, { cause: error });
+	}
+};
