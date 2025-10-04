935fded0f1f89302153cce0488e80c675a9be903
Jean Burellier <jean.burellier@gmail.com>
2025-10-04 20:15:16 +0200

    feat: add organizations method for list members

---
+++ src/organizations/members.js
@@ -0,0 +1,43 @@
+// @GitHub-API: https://docs.github.com/en/rest/orgs/members
+
+import {
+	BASE_COUNTER,
+	DEFAULT_INCREMENT,
+	GITHUB_PAGE_LENGTH,
+	GITHUB_URL,
+} from '../utils/const.js';
+import { GITHUB_TOKEN } from '../utils/token.js';
+import { logger } from '../utils/logger.js';
+
+export async function listMembers(owner) {
+	try {
+		let page = BASE_COUNTER;
+		let continueLoop = true;
+		const members = [];
+
+		while (continueLoop) {
+			const url = `${GITHUB_URL}/orgs/${owner}/members
+				?per_page=${GITHUB_PAGE_LENGTH}&page=${page}`;
+			// eslint-disable-next-line no-await-in-loop
+			const request = await fetch(url, {
+				headers: {
+					Accept: 'application/vnd.github.v3+json',
+					Authorization: `Bearer ${GITHUB_TOKEN}`,
+				},
+			});
+			// eslint-disable-next-line no-await-in-loop
+			const result = await request.json();
+
+			members.push(...result);
+			page = page + DEFAULT_INCREMENT;
+			if (result.length < GITHUB_PAGE_LENGTH) {
+				continueLoop = false;
+			}
+		}
+
+		return members;
+	} catch (error) {
+		logger.error(error);
+		throw new Error(`Error listing members for ${owner}`, { cause: error });
+	}
+};
