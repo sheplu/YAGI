e5bda63adb7e7a74566d049b82c08785aed8a9d2
Jean Burellier <jean.burellier@gmail.com>
2025-10-05 10:14:59 +0200

    feat: add organizations method for list public members

--- src/organizations/members.js
+++ src/organizations/members.js
@@ -41,3 +41,36 @@ export async function listMembers(owner) {
 		throw new Error(`Error listing members for ${owner}`, { cause: error });
 	}
 };
+
+export async function listPublicMembers(owner) {
+	try {
+		let page = BASE_COUNTER;
+		let continueLoop = true;
+		const members = [];
+
+		while (continueLoop) {
+			const url = `${GITHUB_URL}/orgs/${owner}/public_members
+				?per_page=${GITHUB_PAGE_LENGTH}&page=${page}`;
+			// eslint-disable-next-line no-await-in-loop
+			const request = await fetch(url, {
+				headers: {
+					Accept: 'application/vnd.github.v3+json',
+					Authorization: `Bearer ${GITHUB_TOKEN}`,
+				},
+			});
+			// eslint-disable-next-line no-await-in-loop
+			const result = await request.json();
+
+			members.push(...result);
+			page = page + DEFAULT_INCREMENT;
+			if (result.length < GITHUB_PAGE_LENGTH) {
+				continueLoop = false;
+			}
+		}
+
+		return members;
+	} catch (error) {
+		logger.error(error);
+		throw new Error(`Error listing public members for ${owner}`, { cause: error });
+	}
+};
