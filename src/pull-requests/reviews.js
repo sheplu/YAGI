b9c3257583d4e51b6bd2a8e24bac55866d6fb178
Jean Burellier <jean.burellier@gmail.com>
2025-10-05 10:50:14 +0200

    feat: add pull requetes method for list list reviews

---
+++ src/pull-requests/reviews.js
@@ -0,0 +1,46 @@
+// @GitHub-API: https://docs.github.com/en/rest/pulls/reviews
+
+import {
+	BASE_COUNTER,
+	DEFAULT_INCREMENT,
+	GITHUB_PAGE_LENGTH,
+	GITHUB_URL,
+} from '../utils/const.js';
+import { GITHUB_TOKEN } from '../utils/token.js';
+import { logger } from '../utils/logger.js';
+
+export async function listReviews(owner, repository, pullRequestID) {
+	try {
+		let page = BASE_COUNTER;
+		let continueLoop = true;
+		const reviews = [];
+
+		while (continueLoop) {
+			const url = `${GITHUB_URL}/repos/${owner}/${repository}/pulls/${pullRequestID}/reviews
+				?per_page=${GITHUB_PAGE_LENGTH}&page=${page}`;
+			// eslint-disable-next-line no-await-in-loop
+			const request = await fetch(url, {
+				headers: {
+					Accept: 'application/vnd.github.v3+json',
+					Authorization: `Bearer ${GITHUB_TOKEN}`,
+				},
+			});
+			// eslint-disable-next-line no-await-in-loop
+			const result = await request.json();
+
+			reviews.push(...result);
+			page = page + DEFAULT_INCREMENT;
+			if (result.length < GITHUB_PAGE_LENGTH) {
+				continueLoop = false;
+			}
+		}
+
+		return reviews;
+	} catch (error) {
+		logger.error(error);
+		throw new Error(
+			`Error listing reviews for ${owner}/${repository}/${pullRequestID}`,
+			{ cause: error },
+		);
+	}
+};
