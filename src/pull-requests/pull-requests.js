6ae95c8a22b40cf7ea9d71e944ff49bf46db399a
Jean Burellier <jean.burellier@gmail.com>
2025-10-05 10:40:34 +0200

    feat: add pull requetes method for list pull request commits

--- src/pull-requests/pull-requests.js
+++ src/pull-requests/pull-requests.js
@@ -53,3 +53,36 @@ export async function getPullRequest(owner, repository, pullRequestID) {
 
 	return request.json();
 };
+
+export async function listPullRequestCommits(owner, repository, pullRequestID) {
+	try {
+		let page = BASE_COUNTER;
+		let continueLoop = true;
+		const commits = [];
+
+		while (continueLoop) {
+			const url = `${GITHUB_URL}/repos/${owner}/${repository}/pulls/${pullRequestID}/commits
+				?per_page=${GITHUB_PAGE_LENGTH}&page=${page}`;
+			// eslint-disable-next-line no-await-in-loop
+			const request = await fetch(url, {
+				headers: {
+					Accept: 'application/vnd.github.v3+json',
+					Authorization: `Bearer ${GITHUB_TOKEN}`,
+				},
+			});
+			// eslint-disable-next-line no-await-in-loop
+			const result = await request.json();
+
+			commits.push(...result);
+			page = page + DEFAULT_INCREMENT;
+			if (result.length < GITHUB_PAGE_LENGTH) {
+				continueLoop = false;
+			}
+		}
+
+		return commits;
+	} catch (error) {
+		logger.error(error);
+		throw new Error(`Error listing pull requests for ${owner}/${repository}`, { cause: error });
+	}
+};
