c6cf21f784010262672516b5100544ba7c0c5fe6
Jean Burellier <jean.burellier@gmail.com>
2025-10-06 23:15:52 +0200

    feat: add isues method for list issues for repository

---
+++ src/issues/issues.js
@@ -0,0 +1,43 @@
+// @GitHub-API: https://docs.github.com/en/rest/issues/issues
+
+import {
+	BASE_COUNTER,
+	DEFAULT_INCREMENT,
+	GITHUB_PAGE_LENGTH,
+	GITHUB_URL,
+} from '../utils/const.js';
+import { GITHUB_TOKEN } from '../utils/token.js';
+import { logger } from '../utils/logger.js';
+
+export async function listIssues(owner, repository) {
+	try {
+		let page = BASE_COUNTER;
+		let continueLoop = true;
+		const issues = [];
+
+		while (continueLoop) {
+			const url = `${GITHUB_URL}/repos/${owner}/${repository}/issues
+				?per_page=${GITHUB_PAGE_LENGTH}&page=${page}`;
+			// eslint-disable-next-line no-await-in-loop
+			const request = await fetch(url, {
+				headers: {
+					Accept: 'application/vnd.github.v3+json',
+					Authorization: `Bearer ${GITHUB_TOKEN}`,
+				},
+			});
+			// eslint-disable-next-line no-await-in-loop
+			const result = await request.json();
+
+			issues.push(...result);
+			page = page + DEFAULT_INCREMENT;
+			if (result.length < GITHUB_PAGE_LENGTH) {
+				continueLoop = false;
+			}
+		}
+
+		return issues;
+	} catch (error) {
+		logger.error(error);
+		throw new Error(`Error listing issues for ${owner}/${repository}`, { cause: error });
+	}
+};
