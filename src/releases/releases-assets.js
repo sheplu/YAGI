d9564d1e19b38680c5c7963259462c20309d6862
Jean Burellier <jean.burellier@gmail.com>
2025-10-16 23:00:52 +0200

    feat: add releases assets method for list releases assets

--- src/releases/releases-assets.js
+++ src/releases/releases-assets.js
@@ -1,9 +1,13 @@
 // @GitHub-API: https://docs.github.com/en/rest/releases/assets
 
-import { GITHUB_TOKEN } from '../utils/token.js';
 import {
+	BASE_COUNTER,
+	DEFAULT_INCREMENT,
+	GITHUB_PAGE_LENGTH,
 	GITHUB_URL,
 } from '../utils/const.js';
+import { GITHUB_TOKEN } from '../utils/token.js';
+import { logger } from '../utils/logger.js';
 
 /*
  * @doc: https://docs.github.com/en/rest/releases/assets?apiVersion=2022-11-28#get-a-release-asset
@@ -51,4 +55,43 @@ export async function deleteReleaseAsset(owner, repository, assetID) {
 	});
 
 	return request.json();
+};
+
+/*
+ * @doc: https://docs.github.com/en/rest/releases/assets?apiVersion=2022-11-28#list-release-assets
+ */
+export async function listReleasesAsset(owner, repository) {
+	try {
+		let page = BASE_COUNTER;
+		let continueLoop = true;
+		const releases = [];
+
+		while (continueLoop) {
+			const url = `${GITHUB_URL}/repos/${owner}/${repository}/releases/assets
+				?per_page=${GITHUB_PAGE_LENGTH}&page=${page}`;
+			// eslint-disable-next-line no-await-in-loop
+			const request = await fetch(url, {
+				headers: {
+					Accept: 'application/vnd.github.v3+json',
+					Authorization: `Bearer ${GITHUB_TOKEN}`,
+				},
+			});
+			// eslint-disable-next-line no-await-in-loop
+			const result = await request.json();
+
+			releases.push(...result);
+			page = page + DEFAULT_INCREMENT;
+			if (result.length < GITHUB_PAGE_LENGTH) {
+				continueLoop = false;
+			}
+		}
+
+		return releases;
+	} catch (error) {
+		logger.error(error);
+		throw new Error(
+			`Error listing releases assets for ${owner}/${repository}`,
+			{ cause: error },
+		);
+	}
 };
