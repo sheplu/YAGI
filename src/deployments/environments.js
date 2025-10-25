85920d35239658e9e15052d8d88f329e146e5d7b
Jean Burellier <jean.burellier@gmail.com>
2025-10-25 17:51:11 +0200

    feat: add deployments methods for list environments

---
+++ src/deployments/environments.js
@@ -0,0 +1,43 @@
+// @GitHub-API: https://docs.github.com/en/rest/deployments/environments
+
+import {
+	BASE_COUNTER,
+	DEFAULT_INCREMENT,
+	GITHUB_PAGE_LENGTH,
+	GITHUB_URL,
+} from '../utils/const.js';
+import { GITHUB_TOKEN } from '../utils/token.js';
+import { logger } from '../utils/logger.js';
+
+export async function listEnvironments(owner, repository) {
+	try {
+		let page = BASE_COUNTER;
+		let continueLoop = true;
+		const environments = [];
+
+		while (continueLoop) {
+			const url = `${GITHUB_URL}/repos/${owner}/${repository}/environments
+				?per_page=${GITHUB_PAGE_LENGTH}&page=${page}`;
+			// eslint-disable-next-line no-await-in-loop
+			const request = await fetch(url, {
+				headers: {
+					Accept: 'application/vnd.github.v3+json',
+					Authorization: `Bearer ${GITHUB_TOKEN}`,
+				},
+			});
+			// eslint-disable-next-line no-await-in-loop
+			const result = await request.json();
+
+			environments.push(...result);
+			page = page + DEFAULT_INCREMENT;
+			if (result.length < GITHUB_PAGE_LENGTH) {
+				continueLoop = false;
+			}
+		}
+
+		return environments;
+	} catch (error) {
+		logger.error(error);
+		throw new Error(`Error listing environments for ${owner}/${repository}`, { cause: error });
+	}
+};
