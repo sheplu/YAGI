425ccf725880dbd33fa9b53f9be0e0e5b469143f
Jean Burellier <jean.burellier@gmail.com>
2025-10-25 18:50:14 +0200

    feat: add actions methods for list repository secrets

---
+++ src/actions/secrets.js
@@ -0,0 +1,46 @@
+// @GitHub-API: https://docs.github.com/en/rest/actions/secrets
+
+import {
+	BASE_COUNTER,
+	DEFAULT_INCREMENT,
+	GITHUB_PAGE_LENGTH,
+	GITHUB_URL,
+} from '../utils/const.js';
+import { GITHUB_TOKEN } from '../utils/token.js';
+import { logger } from '../utils/logger.js';
+
+/*
+ * @doc: https://docs.github.com/en/rest/actions/secrets?apiVersion=2022-11-28#list-repository-secrets
+ */
+export async function listRepositorySecrets(owner, repository) {
+	try {
+		let page = BASE_COUNTER;
+		let continueLoop = true;
+		const secrets = [];
+
+		while (continueLoop) {
+			const url = `${GITHUB_URL}/repos/${owner}/${repository}/actions/secrets
+				?per_page=${GITHUB_PAGE_LENGTH}&page=${page}`;
+			// eslint-disable-next-line no-await-in-loop
+			const request = await fetch(url, {
+				headers: {
+					Accept: 'application/vnd.github.v3+json',
+					Authorization: `Bearer ${GITHUB_TOKEN}`,
+				},
+			});
+			// eslint-disable-next-line no-await-in-loop
+			const result = await request.json();
+
+			secrets.push(...result);
+			page = page + DEFAULT_INCREMENT;
+			if (result.length < GITHUB_PAGE_LENGTH) {
+				continueLoop = false;
+			}
+		}
+
+		return secrets;
+	} catch (error) {
+		logger.error(error);
+		throw new Error(`Error listing secrets for ${owner}/${repository}`, { cause: error });
+	}
+};
